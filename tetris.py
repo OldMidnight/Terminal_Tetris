70c805fa755f52a89d401326f1a30bdf2842bf65
Moses opebi <abdulmuizz.opebi2@mail.dcu,ie>
2019-03-18 00:11:24 +0000

    Outer grid

--- tetris.py
+++ tetris.py
@@ -11,36 +11,68 @@ Maintainers:
 ######################################################################
 # GLOBAL VARIABLES HERE
 ######################################################################
-running = True
+#running = True
 ######################################################################
 # SHAPES HERE
 ######################################################################
 ######################################################################
 # GRID HERE
+# I looked up the dimesions of  a rectangle.(x,x + 8)
+''' - - - - - - 
+    |         |
+	|         |
+	|         |
+	|	      |
+	|         |
+    - - - - - -
+'''
+
+def outer_grid(x):
+  width = ["-"]
+  lside = ["|"]
+  rside = ["|"]
+  spaces = []
+  i = 0
+  while i < x:
+    width.append("-")
+    spaces.append(" ")
+    i = i + 1
+  print (" ").join(width)
+  
+  j = 0
+  while j < len(width) + 8:
+    print (" ").join(lside) + (" ").join(spaces) + (" ").join(rside)
+    j = j + 1
+  print (" ").join(width)
+
+#Its outputs 1 size bigger than the actual input
+#Incase any of you wanna test it.
+outer_grid(23)
+
 ######################################################################
 ######################################################################
 # FUNCTIONS HERE
 ######################################################################
-def draw_grid()
+#def draw_grid()
 
-def draw_square()
+#def draw_square()
 
-def draw_line()
+#def draw_line()
 
-def draw_complex()
+#def draw_complex()
 
-def get_shape()
+#def get_shape()
 
-def drop_shape()
+#def drop_shape()
 
-def check_loss()
+#def check_loss()
 
-def check_win()
+#def check_win()
 
-def show_menu()
+#def show_menu()
 
 ######################################################################
 # GAME LOOP HERE
 ######################################################################
-while running:
-    show_menu
+#while running:
+ #   show_menu
