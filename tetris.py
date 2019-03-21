7800ce61478193602f4d2c602d3d521d6d2f2076
Moses opebi <abdulmuizz.opebi2@mail.dcu,ie>
2019-03-21 11:20:21 +0000

    grid

--- tetris.py
+++ tetris.py
@@ -7,13 +7,41 @@ Maintainers:
     Moses Opebi
     Tevin Thomas
 '''
+######################################################################
+#Functions
 
+#GRID
+
+def create_grid(size = 21):
+  grid = []
+  grid.append(" " + ("_" * size))
+  i = 1
+  while i < size:
+    grid.append([])
+    grid[i].append("|")
+    j = 0
+    while j < size:
+      grid[i].append(' ')
+      j = j + 1
+    grid[i].append("|")
+    i = i + 1
+  grid.append(" " + ("-" * size))	
+  return grid
+
+create_grid(21)
+
+def draw_grid(grid):
+  for row in grid:
+    print "".join(row)
+
+draw_grid(create_grid(21))
 ######################################################################
 # GLOBAL VARIABLES HERE
 ######################################################################
 #running = True
 ######################################################################
 # SHAPES HERE
+
 ######################################################################
 ######################################################################
 # GRID HERE
@@ -27,27 +55,26 @@ Maintainers:
     - - - - - -
 '''
 
-def outer_grid(x):
-  width = ["-"]
-  lside = ["|"]
-  rside = ["|"]
-  spaces = []
-  i = 0
-  while i < x:
-    width.append("-")
-    spaces.append(" ")
-    i = i + 1
-  print (" ").join(width)
+#def outer_grid(x):
+ # width = ["-"]
+  #lside = ["|"]
+  #rside = ["|"]
+  #spaces = []
+  #i = 0
+  #while i < x:
+   # width.append("-")
+   # spaces.append(" ")
+   # i = i + 1
+  #print (" ").join(width)
   
-  j = 0
-  while j < len(width) + 8:
-    print (" ").join(lside) + (" ").join(spaces) + (" ").join(rside)
-    j = j + 1
-  print (" ").join(width)
+  #j = 0
+  #while j < len(width) + 8:
+   # print (" ").join(lside) + (" ").join(spaces) + (" ").join(rside)
+   # j = j + 1
+  #print (" ").join(width)
 
 #It outputs 1 size bigger than the actual input but it still looks good.
 #Incase any of you wanna test it.
-outer_grid(23)
 
 ######################################################################
 ######################################################################
