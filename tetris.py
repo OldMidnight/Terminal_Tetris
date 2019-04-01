73f2c0528fe8ba3a70a1c43080ab359af1a0f300
OldMidnight <fareedidris20@gmail.com>
2019-04-01 11:01:24 +0100

    Added colors, implemented user movement, added  centering

--- tetris.py
+++ tetris.py
@@ -14,6 +14,19 @@ running = True
 import random
 import os
 import time
+from select import select
+import sys
+
+colors_dict = {
+  'RED': '\033[31m',
+  'GREEN': '\033[32;1m',
+  'BLUE': '\033[34;1m',
+  'YELLOW': '\033[33;1m',
+  'PURPLE': '\033[35;1m',
+  'NONE': '\033[0m'
+}
+
+colors_list = ['RED', 'GREEN', 'BLUE', 'YELLOW', 'PURPLE']
 
 class GridClass():
   '''
@@ -26,6 +39,13 @@ class GridClass():
   Attributes include:
   level_dict: A dictionary containing a key-value relationship between a level and an object containing the properties of that level
   by a '/' to make splitting the shape when we choose it into a list much easier.
+
+  next_shape: A Variable used during the shape_movement method. It lets the method know that the current shape has
+  reached the bottom ans that a new shape should be spawned.
+
+  incoming_shape: A variable used to store the shape that will be spawning next.
+
+  row_sorted: A variable used to let the grid instance know that the row that is full of dots has been sorted and removed and to continue with the regular game processing.
   '''
   
   level_dict = {
@@ -49,6 +69,7 @@ class GridClass():
   next_shape = False
   points = 0
   incoming_shape = ''
+  row_sorted = False
   def __init__(self, grid=[], size=15, level=1):
     '''
     The initial constructor for an instance of a GridClass object.
@@ -73,17 +94,17 @@ class GridClass():
         j = j + 1
       self.grid[i].append("|")
       i = i + 1
-    self.grid.append("-" * self.size)
+    self.grid.append(("_" * self.size))
 
   def draw(self):
     '''
     The draw function of the grid. Anytime this function is called, the screen will be cleared and an updated grid will be redrawn.
     '''
     os.system('clear')
-    print 'S C O R E :' + str(self.points).center(int(os.environ['COLUMNS']))
+    print colors_dict['GREEN'] + str('S C O R E: ' + str(self.points)).center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
     for row in self.grid:
       print "".join(row).center(int(os.environ['COLUMNS']))
-    print 'I N C O M I N G   S H A P E :' + "".join(self.incoming_shape).center(int(os.environ['COLUMNS']))
+    print colors_dict['BLUE'] + str('I N C O M I N G   S H A P E: ' + "".join(self.incoming_shape)).center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
 
   def spawn_shape(self):
     '''
@@ -106,101 +127,146 @@ class GridClass():
     top_row = [' ', ' ', '.', '.', '.', ' ']
     '''
     self.next_shape = False
+
+    color = colors_list[random.randint(0, 3)]
+    current_color = colors_dict[color]
+
     if self.incoming_shape == '':
       current_shape_num = random.randint(0, len(self.level['shape']) - 1)
-      shape = self.level['shape'][current_shape_num].split('/')
+      shape = (current_color + self.level['shape'][current_shape_num] + colors_dict['NONE']).split('/')
     else:
       shape = self.incoming_shape
     next_shape_num = random.randint(0, len(self.level['shape']) - 1)
-    self.incoming_shape = self.level['shape'][next_shape_num].split('/')
+    color = colors_list[random.randint(0, 3)]
+    next_color = colors_dict[color]
+    self.incoming_shape = (next_color + self.level['shape'][next_shape_num] + colors_dict['NONE']).split('/')
     middle_of_row = len(self.grid[1]) / 2
     self.grid[1][middle_of_row:middle_of_row + len(shape)] = shape
-    for a in range(middle_of_row, middle_of_row + len(shape)):
-      self.grid[2][a] = ' '
     self.draw()
-    return self.shape_movement(1, middle_of_row, middle_of_row + len(shape))
+    return self.shape_movement(1, middle_of_row, middle_of_row + len(shape), current_color)
       
-  def shape_movement(self, current_row, shape_start_index, shape_end_index):
+  def shape_movement(self, current_row, shape_start_index, shape_end_index, color):
     next_row = current_row + 1
     if next_row == len(self.grid) - 1:
       self.next_shape = True
+
     if self.next_shape == False:
       for a in range(shape_start_index, shape_end_index):
-        if self.grid[next_row][a] == '.':
+        if self.grid[next_row][a] != ' ':
           self.next_shape = True
+
     if self.next_shape == False:
-      temp_row = self.grid[next_row]
-      self.grid[next_row] = self.grid[current_row]
-      self.grid[current_row] = temp_row
-      time.sleep(self.level['speed'])
+      for a in range(shape_start_index, shape_end_index):
+        self.grid[next_row][a] = '.'
+      for a in range(shape_start_index - 1, shape_end_index + 1):
+        if self.grid[current_row][a] != '|':
+          self.grid[current_row][a] = ' '
+      # temp_row = self.grid[next_row]
+      # self.grid[next_row] = self.grid[current_row]
+      # self.grid[current_row] = temp_row
+      # time.sleep(self.level['speed'])
       self.draw()
+
+      move, o, e = select([sys.stdin], [], [], self.level['speed'])
+
+      if (move):
+        direction = sys.stdin.readline().strip()
+        if direction == 'd':
+          if self.grid[next_row][shape_end_index] == ' ':
+            shape_end_index = shape_end_index + 1
+            shape_start_index = shape_start_index + 1
+        elif direction == 'a':
+          if self.grid[next_row][shape_start_index - 1] == ' ':
+            shape_end_index = shape_end_index - 1
+            shape_start_index = shape_start_index - 1
+
     if self.next_shape:
       return True
     else:
-      self.shape_movement(next_row, shape_start_index, shape_end_index)
+      self.shape_movement(next_row, shape_start_index, shape_end_index, color)
+
+  def remove_point_row(self, row_to_sort):
+    if row_to_sort == 1:
+      for a in range(1, len(self.grid[row_to_sort]) - 1):
+        self.grid[row_to_sort][a] = ' '
+      self.row_sorted = True
+    else:
+      self.grid[row_to_sort] = self.grid[row_to_sort - 1]
+    if self.row_sorted:
+      return True
+    else:
+      return self.remove_point_row(row_to_sort - 1)
+
   def check_status(self):
     if '.' in self.grid[1]:
-        return [1]
+      return [1]
     else:
-        i = 1
-        while i < len(self.grid):
-            if ' ' not in self.grid[i]:
-                return [2, i]
-            i += 1
-        return [0]
+      i = 1
+      while i < len(self.grid) - 1:
+        if ' ' not in self.grid[i]:
+          return [2, i]
+        i += 1
+      return [0]
 
 def select_level():
-    print '            S E L E C T   A   D I F F I C U L T Y :            '.center(int(os.environ['COLUMNS']))
-    print ''.center(int(os.environ['COLUMNS']))
-    print '                    [1]        E A S Y                         '.center(int(os.environ['COLUMNS']))
-    print '                    [2]        M E D I U M                     '.center(int(os.environ['COLUMNS']))
-    print '                    [3]        H A R D                         '.center(int(os.environ['COLUMNS']))
-    print '                    [4]        Y O U   W O N T   W I N         '.center(int(os.environ['COLUMNS']))
-    print ''.center(int(os.environ['COLUMNS']))
+  selected_level = 0
+  print colors_dict['BLUE'] + 'S E L E C T   A   D I F F I C U L T Y :'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+  print ''.center(int(os.environ['COLUMNS']))
+  print colors_dict['GREEN'] +  '    [1]        E A S Y                    '.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+  print colors_dict['YELLOW'] + '    [2]        M E D I U M                '.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+  print colors_dict['RED'] +    '    [3]        H A R D                    '.center(int(os.environ['COLUMNS']))
+  print colors_dict['PURPLE'] + '    [4]        Y O U   W O N T   W I N    '.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+  print ''.center(int(os.environ['COLUMNS']))
+  while selected_level == 0:
     level = raw_input('[ENTER]: ')
     if level == '1':
-        print 'Taking it easy huh?'.center(int(os.environ['COLUMNS']))
-        time.sleep(2)
-        return 1
+      print colors_dict['GREEN'] + 'Taking it easy huh?'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+      time.sleep(2)
+      selected_level = 1
     elif level == '2':
-        print 'Not much of a challenge really.'.center(int(os.environ['COLUMNS']))
-        time.sleep(2)
-        return 2
+      print colors_dict['YELLOW'] + 'Not much of a challenge really.'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+      time.sleep(2)
+      selected_level = 2
     elif level == '3':
-        print "That's More like it!".center(int(os.environ['COLUMNS']))
-        time.sleep(2)
-        return 3
+      print colors_dict['RED'] + "That's More like it!".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+      time.sleep(2)
+      selected_level = 3
     elif level == '4':
-        print 'Abandon hope all ye who have chosen this option...'.center(int(os.environ['COLUMNS']))
-        time.sleep(2)
-        return 4
+      print colors_dict['PURPLE'] + 'Abandon hope all ye who have chosen this option...'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+      time.sleep(2)
+      selected_level = 4
+    else:
+      print colors_dict['RED'] + '[!] Unrecognized Input!' + colors_dict['NONE']
+      print colors_dict['RED'] + '[!] Try Again.' + colors_dict['NONE']
+  return selected_level
+    
 
 def print_menu():
     os.system('clear')
-    print "     ______    ________    ________________     ___ _______    ".center(int(os.environ['COLUMNS']))
-    print "    /      \  /        \  /         /  __  \   /  //  ____/    ".center(int(os.environ['COLUMNS']))
-    print "   /  ____  \/   _____  \/___   ___/  / /  /  /  / \  \__      ".center(int(os.environ['COLUMNS']))
-    print "  /  /   /  /\  /    /  /  /   /  /  _    /  /  /   \    \     ".center(int(os.environ['COLUMNS']))
-    print " /         /  \        /  /   /  /  / \  \  /  / ___/    /     ".center(int(os.environ['COLUMNS']))
-    print "/_________/    \______/  /___/  /__/   \__\/__/ /_______/      ".center(int(os.environ['COLUMNS']))
+    print colors_dict['GREEN'] + "       ______    ________    ________________     ___ _______    ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['GREEN'] + "      /      \  /        \  /         /  __  \   /  //  ____/    ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['GREEN'] + "     /  ____  \/   _____  \/___   ___/  / /  /  /  / \  \__      ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['GREEN'] + "    /  /   /  /\  /    /  /  /   /  /  _    /  /  /   \    \     ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['GREEN'] + "   /         /  \        /  /   /  /  / \  \  /  / ___/    /     ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['GREEN'] + "  /_________/    \______/  /___/  /__/   \__\/__/ /_______/      ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
     print ''.center(int(os.environ['COLUMNS']))
-    print '                   C A 2 7 8   P R O J E C T                   '.center(int(os.environ['COLUMNS']))
-    print '_______________________________________________________________'.center(int(os.environ['COLUMNS']))
-    print '                        M A D E   B Y :                        '.center(int(os.environ['COLUMNS']))
-    print '                    F A R E E D   I D R I S                    '.center(int(os.environ['COLUMNS']))
-    print '                     M O S E S   O P E B I                     '.center(int(os.environ['COLUMNS']))
-    print '                     K E V I N   D O Y L E                     '.center(int(os.environ['COLUMNS']))
-    print '                    T E V I N   T H O M A S                    '.center(int(os.environ['COLUMNS']))
-    print '_______________________________________________________________'.center(int(os.environ['COLUMNS']))
-    print ''.center(int(os.environ['COLUMNS']))
-    print '                 [1] New Game      [2] Highscores              '.center(int(os.environ['COLUMNS']))
-    print '                            [3] Quit                           '.center(int(os.environ['COLUMNS']))
-    print ''.center(int(os.environ['COLUMNS']))
+    print colors_dict['GREEN'] + 'C A 2 7 8   P R O J E C T'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['YELLOW'] + '_______________________________________________________________'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['BLUE'] + 'M A D E   B Y :'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['BLUE'] + 'F A R E E D   I D R I S'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['BLUE'] + 'M O S E S   O P E B I'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['BLUE'] + 'K E V I N   D O Y L E'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['BLUE'] + 'T E V I N   T H O M A S'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['YELLOW'] + '_______________________________________________________________'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print ''.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['GREEN'] + '[1] New Game      [2] Highscores'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print colors_dict['GREEN'] + '[3] Quit'.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+    print ''.center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
     option = input('[ENTER]: ')
     if option in [1, 2, 3]:
         return option
     else:
-        print '[!] Unrecognized Input!'
+        print colors_dict['RED'] + '[!] Unrecognized Input!' + colors_dict['NONE']
         return 3
 
 def start_process(difficulty):
@@ -212,19 +278,27 @@ def start_process(difficulty):
     game_status = status[0]
     if game_status == 2:
       grid.points += 1
-      #grid.remove_point_row(status[1])
-      #grid.row_sorted = False
+      grid.remove_point_row(status[1])
+      grid.row_sorted = False
       game_status = 0
   return game_status
 
 def show_game_over():
-  print ""
+  print colors_dict['RED'] + "   ________    ______    _________________________    _________    ____    _____________ ______    ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+  print colors_dict['RED'] + "  /  _____/   / __   \  /  ___   ___   /  _______/   /   ____  \  /   /   /  /  _______//  __  \   ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+  print colors_dict['RED'] + " /  /   ___  / /__    \/  /  /  /  /  /  /______    /   /    /  \/   /   /  /  /______ /  / /  /   ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+  print colors_dict['RED'] + " \   \  \  \/  ___    /  /  /  /  /  /  _______/    \       /   /   /   /  /  _______//  _    /    ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+  print colors_dict['RED'] + "  \   \_/  /  /   /  /  /  /  /  /  /  /______       \         /\   \__/  /  /______ /  / \   \    ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+  print colors_dict['RED'] + "   \______/__/   /__/__/  /__/  /__/_________/        \_______/  \_______/_________//__/   \___\   ".center(int(os.environ['COLUMNS'])) + colors_dict['NONE']
+
+
 while running:
     choice = print_menu()
     if choice == 1:
         level = select_level()
         start_process(level)
         show_game_over()
+        time.sleep(5)
         running = False
     elif choice == 3:
         show_highscores()
